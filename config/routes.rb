c5f3e26183dd5758508bc1193227d42412f650a1
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-04-13 02:05:39 +0900

    (13)ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, olny[:index, :show, :edit]
+  resources :books, only[:new, :index, :show, :edit]
 end
