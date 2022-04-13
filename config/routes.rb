637eff764333194bc2804489cd96c1cd9b42c1e4
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-04-14 02:15:45 +0900

    (18)aboutページのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get "home/about" => "homes#about"
   resources :users, only: [:index, :show, :edit]
   resources :books, only: [:new, :index, :show, :edit]
 end
