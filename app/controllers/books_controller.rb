5c90f1b42e45e9e94c5ab90f903a5f08e7ac74cf
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-04-16 20:46:16 +0900

    (23)bookのcreate機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,24 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def index
-    @user = current_user
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
