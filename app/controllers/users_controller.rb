6e13aa66416a6d3984529f4de87b258ba9063f2f
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-04-17 10:57:55 +0900

    (26)userのshowページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    # @books = Book.where(user_id: @user.id)
+    @books = @user.book
   end
 
   def edit
