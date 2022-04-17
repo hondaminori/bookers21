f981a4c9bb9f3dd8b8caae84956c907b9f4cc2e4
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-04-18 03:48:50 +0900

    (36)新規投稿が成功したとき

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: 'about'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users
   resources :books
 end
